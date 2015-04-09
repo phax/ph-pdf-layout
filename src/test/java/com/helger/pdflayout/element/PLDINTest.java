bc9671b0b8c738ad7df14dcceb3ff1376d73209e
phax <philip@helger.com>
2015-04-09 21:44:00 +0200

    Using PLConvert

--- src/test/java/com/helger/pdflayout/element/PLDINTest.java
+++ src/test/java/com/helger/pdflayout/element/PLDINTest.java
@@ -24,13 +24,8 @@ import org.junit.rules.TestRule;
 import com.helger.commons.io.file.FileUtils;
 import com.helger.commons.mock.DebugModeTestRule;
 import com.helger.pdflayout.PDFCreationException;
+import com.helger.pdflayout.PLConvert;
 import com.helger.pdflayout.PageLayoutPDF;
-import com.helger.pdflayout.element.PLHBox;
-import com.helger.pdflayout.element.PLPageSet;
-import com.helger.pdflayout.element.PLSpacerX;
-import com.helger.pdflayout.element.PLSpacerY;
-import com.helger.pdflayout.element.PLText;
-import com.helger.pdflayout.element.PLVBox;
 import com.helger.pdflayout.spec.FontSpec;
 import com.helger.pdflayout.spec.PDFFont;
 import com.helger.pdflayout.spec.WidthSpec;
@@ -43,30 +38,27 @@ public final class PLDINTest
   @Test
   public void testDINLetter () throws PDFCreationException
   {
-    final float fMMToUnits = 1 / (10 * 2.54f) * 72;
     final FontSpec r10 = new FontSpec (PDFFont.REGULAR, 10);
 
     final PLPageSet aPS1 = new PLPageSet (PDPage.PAGE_SIZE_A4);
     {
       final PLVBox aVBox = new PLVBox ();
       final PLHBox aHBox = new PLHBox ();
-      aHBox.addColumn (new PLSpacerX (), WidthSpec.abs (20 * fMMToUnits));
+      aHBox.addColumn (new PLSpacerX (), WidthSpec.abs (PLConvert.mm2units (20)));
       {
         final PLVBox aWindow = new PLVBox ();
-        aWindow.addRow (new PLSpacerY (42 * fMMToUnits));
-        aWindow.addRow (new PLText ("Hr. MaxMustermann\nMusterstraße 15\nA-1010 Wien", r10).setMinSize (90 * fMMToUnits,
-                                                                                                        45 * fMMToUnits)
-                                                                                           .setMaxSize (90 * fMMToUnits,
-                                                                                                        45 * fMMToUnits));
-        aWindow.addRow (new PLSpacerY (12 * fMMToUnits));
-        aHBox.addColumn (aWindow, WidthSpec.abs (90 * fMMToUnits));
+        aWindow.addRow (new PLSpacerY (PLConvert.mm2units (42)));
+        aWindow.addRow (new PLText ("Hr. MaxMustermann\nMusterstraße 15\nA-1010 Wien", r10).setExactSize (PLConvert.mm2units (90),
+                                                                                                          PLConvert.mm2units (45)));
+        aWindow.addRow (new PLSpacerY (PLConvert.mm2units (12)));
+        aHBox.addColumn (aWindow, WidthSpec.abs (PLConvert.mm2units (90)));
       }
       aHBox.addColumn (new PLSpacerX (), WidthSpec.star ());
       aVBox.addRow (aHBox);
       aPS1.addElement (aVBox);
     }
-    aPS1.addElement (new PLSpacerY (99 * fMMToUnits));
-    aPS1.addElement (new PLSpacerY (98.5f * fMMToUnits));
+    aPS1.addElement (new PLSpacerY (PLConvert.mm2units (99)));
+    aPS1.addElement (new PLSpacerY (PLConvert.mm2units (98.5f)));
 
     final PageLayoutPDF aPageLayout = new PageLayoutPDF ().setDebug (true);
     aPageLayout.addPageSet (aPS1);
