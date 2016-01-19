72e73e0d01d89e3bc2b01acf5667382ef61e2399
Philip Helger <philip@helger.com>
2016-01-19 19:53:09 +0100

    Minimum version to get the original error

--- src/test/java/com/helger/pdflayout/supplementary/issues/MainIssue3162.java
+++ src/test/java/com/helger/pdflayout/supplementary/issues/MainIssue3162.java
@@ -18,14 +18,10 @@ package com.helger.pdflayout.supplementary.issues;
 
 import java.io.IOException;
 
-import org.apache.fontbox.ttf.TTFParser;
-import org.apache.fontbox.ttf.TrueTypeFont;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentHelper;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
 import com.helger.font.open_sans.EFontResource;
@@ -34,40 +30,25 @@ public final class MainIssue3162
 {
   public static void main (final String [] args) throws IOException
   {
-    final String s = "This is a test";
-    final String file = "pdf/issue-3162.pdf";
-
     final PDDocument doc = new PDDocument ();
     try
     {
       final PDPage page = new PDPage (PDRectangle.A4);
       doc.addPage (page);
 
-      final TrueTypeFont ttf = new TTFParser ().parse (EFontResource.OPEN_SANS_NORMAL.getFontResource ()
-                                                                                     .getInputStream ());
-      final PDFont font = PDType0Font.load (doc, ttf, true);
-      final PDFont font2 = PDType0Font.load (doc, ttf, true);
-      PDDocumentHelper.handleFontSubset (doc, font);
-      PDDocumentHelper.handleFontSubset (doc, font2);
-
       final PDPageContentStream contents = new PDPageContentStream (doc, page);
       try
       {
         contents.beginText ();
-        contents.setFont (font, 12);
-        contents.newLineAtOffset (100, 700);
-        contents.showText (s);
-        contents.setFont (font2, 12);
-        contents.newLineAtOffset (0, -14);
-        contents.showText (s + " line 2");
+        contents.setFont (PDType0Font.load (doc, EFontResource.OPEN_SANS_NORMAL.getFontResource ().getInputStream ()),
+                          12);
         contents.endText ();
       }
       finally
       {
         contents.close ();
       }
-
-      doc.save (file);
+      // No need to save
     }
     finally
     {
