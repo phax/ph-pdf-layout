b0873dc9073bbfd3e5144f260fb5a2f7e2bf02a8
Philip Helger <philip@helger.com>
2019-03-06 11:29:08 +0100

    Updated to PDFBox 2.0.14

--- src/main/java/com/helger/pdflayout4/config/ThirdPartyModuleProvider_ph_pdf_layout.java
+++ src/main/java/com/helger/pdflayout4/config/ThirdPartyModuleProvider_ph_pdf_layout.java
@@ -37,7 +37,7 @@ public final class ThirdPartyModuleProvider_ph_pdf_layout implements IThirdParty
   public static final IThirdPartyModule PDFBOX = new ThirdPartyModule ("Apache PDFBox",
                                                                        "Apache",
                                                                        ELicense.APACHE2,
-                                                                       new Version (2, 0, 13),
+                                                                       new Version (2, 0, 14),
                                                                        "http://pdfbox.apache.org/");
 
   @Nullable
